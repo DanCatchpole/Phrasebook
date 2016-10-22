b28360d2afaa6ced499fcc23c803fe5b8f7baf7f
Daniel Catchpole <daniel.catchpole@live.com>
2016-10-22 17:34:06 +0100

    testing

--- js/search.js
+++ js/search.js
@@ -34,7 +34,7 @@ $(function(){
                 $wordBlock.append($lang);
                 $wordBlock.append($padding1);
                 $wordBlock.append($translations);
-                if (!$("#category")) {
+                if (!$(".category h1")) {
                     var $padding2 = $("<span>", {class: "padding"});
                     var $category = $("<span>", {class: "category"});
                     $category.text(elem.catName);
